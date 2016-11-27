76a8a6d8eeef24b6141a0ba13b341b0f4094086c
iloveupal <danil.sorokin1995@gmail.com>
2016-11-27 02:57:10 +0200

    hook

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var app = require('express')();
 
+var prettyjson = require('prettyjson');
+
 app.use(require('body-parser').json());
 
 app.post('/payload', function(req, res) {
-    console.log(req.body);
+    console.log(prettyjson.render(req.body));
     res.send('ok');
 });
 
